1b410de716175c33d0cdea7f8cacc8a284d43295
Dhruv Charkhawala <dv.charkhawala@gmail.com>
2024-07-12 16:11:39 +0530

    order url changes

--- src/actions/orderAction.js
+++ src/actions/orderAction.js
@@ -16,7 +16,7 @@ export const placeOrder = (subtotal) => async (dispatch, getState) => {
 			handler: async (response) => {
 				try {
 					const { data } = await axios.post(
-						'http://localhost:8080/api/payment/verifypayment',
+						'https://pizza-backend-gules.vercel.app/api/payment/verifypayment',
 						//post user to backend
 						{
 							paymentId: response.razorpay_payment_id,
@@ -54,7 +54,7 @@ export const placeOrder = (subtotal) => async (dispatch, getState) => {
 
 	try {
 		const { data } = await axios.post(
-			'http://localhost:8080/api/payment/orders',
+			'https://pizza-backend-gules.vercel.app/api/payment/orders',
 			{ subtotal },
 		);
 
@@ -83,7 +83,7 @@ export const getUserOrders = () => async (dispatch, getState) => {
 
 	try {
 		const response = await axios.post(
-			'http://localhost:8080/api/payment/getuserorders',
+			'https://pizza-backend-gules.vercel.app/api/payment/getuserorders',
 			{ userId: user._id },
 		);
 		console.log(response);
@@ -98,7 +98,7 @@ export const getAllOrders = () => async (dispatch) => {
 	dispatch({ type: 'GET_ALL_ORDERS_REQUEST' });
 	try {
 		const response = await axios.get(
-			'http://localhost:8080/api/payment/getallorders',
+			'https://pizza-backend-gules.vercel.app/api/payment/getallorders',
 		);
 		console.log(response);
 		dispatch({ type: 'GET_ALL_ORDERS_SUCCESS', payload: response.data });
